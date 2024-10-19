7fbef0f83ef04da1d37fee70ea14d0d08359bc21
parnelltse <parnell.tse@gmail.com>
2024-10-19 14:56:32 -0700

    initial commit

---
+++ matching game/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <header>
+        <div>
+          <h1>
+            Matching Game
+          </h1>
+          <div>
+            <button className='icon'>profile</button>
+            <button className='icon'>settings</button>
+          </div>
+        </div>
+      </header>
+
+    </div>
+  )
+}
+
+export default App
