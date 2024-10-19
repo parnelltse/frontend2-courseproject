b61bdb482928fb5098524503dc160f6604ad5a84
parnelltse <parnell.tse@gmail.com>
2024-10-19 16:46:39 -0700

    routing landing to create and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
-import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom'
-import { Landing } from './pages/landing'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Landing } from './pages/landing';
+import Create from './pages/create'; 
+import Login from './pages/login'
 
 function App() {
-
-
   return (
-   <Routers>
-    <Routes>
-      <Route path="/" element={<Landing/>} />
-    </Routes>
-   </Routers>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,18 @@
+import React from 'react';
 
-export default function Login () {
-    return(
-        <>
-        </>
-    )
+export default function Login() {
+    return (
+        <div>
+            <header>
+                <h1>Matching Game</h1>
+                <h2>Account Login</h2>
+            </header>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
+
