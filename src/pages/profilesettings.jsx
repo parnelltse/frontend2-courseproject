22e2e77f9569a0c07edced3ccdbf4090cfd42be3
Boi San Ly <144407141+SandyLy453@users.noreply.github.com>
2024-11-17 01:32:36 -0800

    Update profilesettings.jsx

--- src/pages/profilesettings.jsx
+++ src/pages/profilesettings.jsx
@@ -68,7 +68,7 @@ const ProfileSettings = () => {
         <button className="scores-button" onClick={() => navigate('/gamehistory')}>
           Scores
         </button>
-        <button className="logout-button" onClick={() => navigate('/landing')}>Logout</button>
+        <button className="logout-button" onClick={() => navigate('/')}>Logout</button>
       </div>
     </div>
   );
